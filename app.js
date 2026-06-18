101260f68c877d96ea0f9963ae72a619df29acd1
Chanchal <raochanchal1366@gmail.com>
2026-06-18 22:57:55 +0530

    01-folder

---
+++ app.js
@@ -0,0 +1,4 @@
+export const arr = [10, 20, 30, 40];
+const user = 'Sarthak'
+
+export default user
---
+++ script.js
@@ -0,0 +1,4 @@
+import username from './app.js'
+import {arr} from './app.js'
+console.log(username);
+console.log(arr)
